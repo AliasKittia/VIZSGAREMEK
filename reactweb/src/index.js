3628fb213f7771f473e4bb275ae80863e8302d3f
AliasKittia <147054637+AliasKittia@users.noreply.github.com>
2024-12-10 09:37:23 +0100

    Replanning v1
    Delete everything in react, and follder managment for the next sprints

--- reactweb/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client'; // Helyes import a React 18-hoz
-import HomePages from './HomePage';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<HomePages />);
