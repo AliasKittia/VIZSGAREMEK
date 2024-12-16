0a66c3a071ba3f9244343e087eb97f002d838f1d
AliasKittia <147054637+AliasKittia@users.noreply.github.com>
2024-12-16 08:50:29 +0100

    update

--- sprint_2/code/tftwebapi/obj/Debug/net8.0/tftwebapi.AssemblyInfo.cs
+++ sprint_2/code/tftwebapi/obj/Debug/net8.0/tftwebapi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tftwebapi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86d1de49e2ed6d2cf2d85ecb22ba156500c98dfc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d8679f3d5daa06f34c2be91968eb27131e4b08f")]
 [assembly: System.Reflection.AssemblyProductAttribute("tftwebapi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tftwebapi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
