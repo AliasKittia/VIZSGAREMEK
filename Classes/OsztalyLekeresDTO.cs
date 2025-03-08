1f67b7c4f712280acad650bc764976e2b5a6fed0
MimitheEmili22 <mozsikel@gmail.hu>
2025-03-08 21:36:26 +0100

    Ez még változni fog OsztalyDTO

--- Classes/OsztalyLekeresDTO.cs
+++ Classes/OsztalyLekeresDTO.cs
@@ -2,13 +2,18 @@
 {
     public class OsztalyLekeresDTO
     {
-        public int ClassId { get; set; }
-
         public string ClassName { get; set; } = null!;
-
         public string? BasicEffect { get; set; }
-
         public string? Classimageblob { get; set; }
+        public int Level { get; set; }
+        public int? CharacterCount { get; set; }
+        public string? BonusEffect { get; set; }
+
 
     }
+
+
+
+
+
 }
