25397d247e72bf687838fccd2e9d4803acbfeea2
MimitheEmili22 <mozsikel@gmail.hu>
2025-03-09 21:56:46 +0100

    OsztályDTO megvan végre gecc

--- Classes/OsztalyLekeresDTO.cs
+++ Classes/OsztalyLekeresDTO.cs
@@ -2,18 +2,15 @@
 {
     public class OsztalyLekeresDTO
     {
+        public int ClassID { get; set; }
         public string ClassName { get; set; } = null!;
         public string? BasicEffect { get; set; }
         public string? Classimageblob { get; set; }
+
         public int Level { get; set; }
         public int? CharacterCount { get; set; }
         public string? BonusEffect { get; set; }
-
-
     }
-
-
-
 
 
 }
