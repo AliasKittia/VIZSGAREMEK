6acdc575659900ab8e8715e2f6f09b2e54c9f305
AliasKittia <147054637+AliasKittia@users.noreply.github.com>
2025-03-14 08:46:33 +0100

    Delete sprint_3 directory

--- sprint_3/code/tftwebapi/Models/PostCharacterClass.cs
+++
@@ -1,6 +0,0 @@
-namespace tftwebapi.Models;
-
-public class PostCharacterClass{
-    public required int CharacterId { get; set; }
-    public required int ClassId { get; set; }
-}
--- sprint_3/code/tftwebapi/Models/PostClass.cs
+++
@@ -1,9 +0,0 @@
-namespace tftwebapi.Models
-{
-    public class PostClass
-    {
-        public required int ClassId { get; set; }
-        public required string ClassName { get; set; }
-        public required string BasicEffect { get; set; }
-    }
-}
