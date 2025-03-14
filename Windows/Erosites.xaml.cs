ed641aed9b5baecd5ecda85e40dfcbafffe91328
MimitheEmili <147054667+MimitheEmili@users.noreply.github.com>
2025-03-14 10:50:57 +0100

    Erősítések ablak megvan teljesen

--- Windows/Erosites.xaml.cs
+++ Windows/Erosites.xaml.cs
@@ -23,6 +23,7 @@ namespace Karbantarto.Windows
     public partial class Erosites : Window
     {
 
+        Menu MenuAblak;
         Karakterek KarakterAblak;
         Osztalyok OsztalyAblak;
         Targyak TargyAblak;
@@ -63,44 +64,51 @@ namespace Karbantarto.Windows
 
         private void FoOldalBTN_Click(object sender, RoutedEventArgs e)
         {
-
+            MenuAblak = new Menu();
+            MenuAblak.Show();
+            this.Close();
         }
 
         private void KarakterekBTN_Click(object sender, RoutedEventArgs e)
         {
             KarakterAblak = new Karakterek();
             KarakterAblak.Show();
-
+            this.Close();
         }
 
         private void ClassokBTN_Click(object sender, RoutedEventArgs e)
         {
             OsztalyAblak = new Osztalyok();
             OsztalyAblak.Show();
+            this.Close();
         }
 
         private void ItemekBTN_Click(object sender, RoutedEventArgs e)
         {
             TargyAblak = new Targyak();
             TargyAblak.Show();
+            this.Close();
         }
 
         private void AnomaliakBTN_Click(object sender, RoutedEventArgs e)
         {
             AnomaliaAblak = new Anomaliak();
             AnomaliaAblak.Show();
+            this.Close();
         }
 
         private void TraitekBTN_Click(object sender, RoutedEventArgs e)
         {
             ErositesAblak = new Erosites();
             ErositesAblak.Show();
+            this.Close();
         }
 
         private void CsapatTervezoBTN_Click(object sender, RoutedEventArgs e)
         {
             CsapatepitoAblak = new Csapatepito();
             CsapatepitoAblak.Show();
+            this.Close();
 
         }
 
@@ -111,12 +119,6 @@ namespace Karbantarto.Windows
             {
                 Application.Current.Shutdown();
             }
-        }
-
-        private void KijelentkezesBTN_Click(object sender, RoutedEventArgs e)
-        {
-
-
         }
     }
 }
