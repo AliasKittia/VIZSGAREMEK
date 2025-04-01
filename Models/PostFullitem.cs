03b1853dc1400452cff7d5513e369a15c2f6a42c
AliasKittia <147054637+AliasKittia@users.noreply.github.com>
2025-04-01 07:52:18 +0200

    Fix ICollection type declaration in PostFullitem model

--- Models/PostFullitem.cs
+++ Models/PostFullitem.cs
@@ -11,6 +11,6 @@ namespace tftwebapinew.Models
         public required string ActiveEffect { get; set; }
         public required string Fullitemimagineblob { get; set; }
 
-        public virtual ICollection<> FullitemsPartialitems { get; set; } = new List<FullitemsPartialitem>();
+        public virtual ICollection<FullitemsPartialitem> FullitemsPartialitems { get; set; } = new List<FullitemsPartialitem>();
     }
 }
