cb6bcffd7f545da18b4de20cb8e668bc3b6bd672
AliasKittia <147054637+AliasKittia@users.noreply.github.com>
2025-04-03 08:16:37 +0200

    Rename ICollection property in PostFullitem model for consistency

--- Models/PostFullitem.cs
+++ Models/PostFullitem.cs
@@ -11,6 +11,6 @@ namespace tftwebapinew.Models
         public required string ActiveEffect { get; set; }
         public required string Fullitemimageblob { get; set; }
 
-        public virtual ICollection<PostFullitem_Partialitem> FullitemsPartialitems { get; set; } = new List<PostFullitem_Partialitem>();
+        public virtual ICollection<PostFullitem_Partialitem> Fullitem_Partialitems { get; set; } = new List<PostFullitem_Partialitem>();
     }
 }
