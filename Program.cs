142b133648538f9adf0bbbaf6faa285a65454e0a
MimitheEmili <147054667+MimitheEmili@users.noreply.github.com>
2025-04-03 08:10:31 +0200

    Scaffold

--- Program.cs
+++ Program.cs
@@ -59,3 +59,4 @@ namespace tftwebapinew
         }
     }
 }
+// Scaffold-DbContext "SERVER=localhost;PORT=3306;DATABASE=tftdatabase;USER=root;PASSWORD=;SSL MODE=none;" mysql.entityframeworkcore -outputdir Models -f
