a2d4a56b4e530f9640fd929b049499e812aab9ea
AliasKittia <147054637+AliasKittia@users.noreply.github.com>
2025-04-03 08:19:46 +0200

    Add JsonIgnore attribute to Fullitem_Partialitems property in PostFullitem model

--- Models/PostFullitem.cs
+++ Models/PostFullitem.cs
@@ -1,3 +1,5 @@
+using System.Text.Json.Serialization;
+
 namespace tftwebapinew.Models
 {
     public class PostFullitem{
@@ -10,7 +12,7 @@ namespace tftwebapinew.Models
          public required string? bonuseffect2 { get; set; }
         public required string ActiveEffect { get; set; }
         public required string Fullitemimageblob { get; set; }
-
+        [JsonIgnore]
         public virtual ICollection<PostFullitem_Partialitem> Fullitem_Partialitems { get; set; } = new List<PostFullitem_Partialitem>();
     }
 }
