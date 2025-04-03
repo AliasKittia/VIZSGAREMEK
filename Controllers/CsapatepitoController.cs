d6d09c66f6c717200d5f92c65b307e477e99a117
MimitheEmili22 <mozsikel@gmail.hu>
2025-04-03 20:00:10 +0200

    Csapatépítő Controller és DTO

---
+++ Controllers/CsapatepitoController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using tftwebapinew.Database;
+using tftwebapinew.DTO;
+using tftwebapinew.Models;
+
+namespace tftwebapinew.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CsapatepitoController : ControllerBase
+    {
+        private readonly tftdatabaseContext _context;
+
+        public CsapatepitoController(tftdatabaseContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CsapatepitoBoardHexDTO>>> GetClasses()
+        {
+            return Ok(Ok(await _context.boardHexes
+                .AsNoTracking()
+                .Include(b => b.Id)
+                .Select(b => new CsapatepitoBoardHexDTO
+                {
+                    Id = b.Id,
+                    Board_id = b.Board_id,
+                    CharacterID = b.CharacterID,
+                    hex_x = b.hex_x,
+                    hex_y = b.hex_y
+                })
+                .ToListAsync()));
+        }
+    }
+}
---
+++ DTO/CsapatepitoDTO.cs
@@ -0,0 +1,52 @@
+﻿using System.Text.Json.Serialization;
+using tftwebapinew.Models;
+
+namespace tftwebapinew.DTO
+{
+    public class CsapatepitoBoardHexDTO
+    {
+        public required int Id { get; set; }
+        public required int Board_id { get; set; }
+        public required int CharacterID { get; set; }
+        public required int hex_x { get; set; }
+        public required int hex_y { get; set; }
+    }
+    public class CsapatepitoBoardDTO
+    {
+        public int Board_id { get; set; }
+        public int id { get; set; }
+        public string? Boardname { get; set; }
+        public int PostUserId { get; set; }
+
+        [JsonIgnore]
+        public virtual PostUser? PostUser { get; set; }
+    }
+    public class CsapatepitoOsztalyDTO
+    {
+        public required int ClassId { get; set; }
+        public required string ClassName { get; set; }
+        public required string BasicEffect { get; set; }
+
+        public virtual ICollection<PostClassLevelBonus> Classlevelbonus { get; set; } = new List<PostClassLevelBonus>();
+
+        public virtual ICollection<PostCharacter> Characters { get; set; } = new List<PostCharacter>();
+    }
+
+    public class CsapatepitoCharacterDTO
+    {
+        public required int CharacterID { get; set; }
+        public required string CharacterName { get; set; }
+
+        [JsonIgnore]
+        public virtual ICollection<PostBoardHex> BoardHexes { get; set; } = new List<PostBoardHex>();
+        public virtual ICollection<PostClass> Classes { get; set; } = new List<PostClass>();
+    }
+
+    public class CsapatepitoUserDTO
+    {
+        [JsonIgnore]
+        public virtual ICollection<PostBoard> Boards { get; set; } = new List<PostBoard>();
+        [JsonIgnore]
+        public virtual PostPermission Permission { get; set; } = null!;
+    }
+}
