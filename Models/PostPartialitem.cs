80358b27d5a0390799f60bd024c6d8a9da95afc3
AliasKittia <147054637+AliasKittia@users.noreply.github.com>
2025-04-03 08:27:50 +0200

    Fix typo in property name for halfitemimageBlob in PostPartialitem model

--- Models/PostPartialitem.cs
+++ Models/PostPartialitem.cs
@@ -4,6 +4,6 @@ namespace tftwebapinew.Models
          public required int partial_item_id { get; set; }
         public required string name { get; set; }
         public required string effect { get; set; }
-        public required string halfitemimagineBlob { get; set; }
+        public required string halfitemimageBlob { get; set; }
     }
 }
