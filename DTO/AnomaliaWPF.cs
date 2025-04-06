1fabbb7ffc795cec2f78778784e76c4c32661838
MimitheEmili22 <mozsikel@gmail.hu>
2025-04-06 18:42:09 +0200

    Csak 1-1 controller és dto

---
+++ DTO/AnomaliaWPF.cs
@@ -0,0 +1,22 @@
+﻿namespace tftwebapinew.DTO
+{
+    public class AnomaliaLekeresWithPermissionDTO
+    {
+        public class AnomaliaWPF
+        {
+            public required int AnomalyId { get; set; }
+            public required string AnomalyName { get; set; }
+            public required string AnomalyEffect { get; set; }
+
+            // Felhasználó adatok
+            public required int UserId { get; set; }
+            public required string UserName { get; set; }
+            public required int PermissionLevel { get; set; }
+            public required string PermissionName { get; set; }
+
+            // Kényelmi property a szerkeszthetőséghez
+            public bool IsEditable => PermissionLevel == 2;
+        }
+
+    }
+}
