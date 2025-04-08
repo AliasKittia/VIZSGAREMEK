1f77f28b6077ebff1834d88948a00136688be98d
MimitheEmili22 <mozsikel@gmail.hu>
2025-04-08 19:57:58 +0200

    Kész az utolsó oldal

---
+++ Classes/KarakterAdatokDTO.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Karbantarto.Classes
+{
+    public class CsapatepitoKarakterDTO
+    {
+        public required int CharacterID { get; set; }
+        public required string CharacterName { get; set; }
+        public required string AbilityName { get; set; }
+        public required string Ability { get; set; }
+        public required int Cost { get; set; }
+        public required int Health { get; set; }
+        public required int Health1 { get; set; }
+        public required int Health2 { get; set; }
+        public required double AttackSpeed { get; set; }
+        public required int Damage { get; set; }
+        public required int Damage1 { get; set; }
+        public required int Damage2 { get; set; }
+        public required int AbilityPower { get; set; }
+        public required int ManaStart { get; set; }
+        public required int ManaMax { get; set; }
+        public required int Armor { get; set; }
+        public required int MagicResist { get; set; }
+        public required int Range { get; set; }
+        public required string? Characterimageblob { get; set; }
+        public List<CsapatepitoOsztalyDTO> Karakterek { get; set; } = new List<CsapatepitoOsztalyDTO>();
+    }
+
+    public class CsapatepitoOsztalyDTO
+    {
+        public int ClassID { get; set; }
+        public string ClassName { get; set; } = null!;
+        public string? BasicEffect { get; set; }
+        public string? Classimageblob { get; set; }
+
+    }
+
+}
--- Windows/Csapatepito.xaml.cs
+++ Windows/Csapatepito.xaml.cs
@@ -1,6 +1,10 @@
 ﻿using Karbantarto.Classes;
+using Karbantarto.Classes.ProjectName_Backend.DTOs;
+using Newtonsoft.Json;
+using ProjectName_Backend.DTOs;
 using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -14,8 +18,13 @@ namespace Karbantarto.Windows
     /// </summary>
     public partial class Csapatepito : Window
     {
-        private int _boardId = 1; // A tábla ID-ja, amit betöltünk/mentünk
-        private List<HexCellDTO> _hexCells = new List<HexCellDTO>();
+        public static List<OsztalyDTO> OsztalyLista { get; set; } = new List<OsztalyDTO>();
+
+        public static List<CsapatepitoKarakterDTO> KarakterLista { get; set; } = new List<CsapatepitoKarakterDTO>();
+
+        private CsapatepitoKarakterDTO? SelectedCharacter;
+
+
         Menu MenuAblak;
         Karakterek KarakterAblak;
         Osztalyok OsztalyAblak;
@@ -24,131 +33,61 @@ namespace Karbantarto.Windows
         Erosites ErositesAblak;
         Csapatepito CsapatepitoAblak;
 
+        public static HttpClient sharedClient = new()
+        {
+            BaseAddress = new Uri("http://localhost:5166/api/KarakterClass"),
+        };
+
         public Csapatepito()
         {
             InitializeComponent();
-            CreateHexagonalGrid(4, 7); // 4 sor és 7 oszlop
-            LoadBoardData(); // Betöltjük az adatbázisból a karaktereket
+            LoadKarakterekAsync(); // pl. saját metódus
+
+            CharacterListBox.ItemsSource = KarakterLista;
         }
 
-        private void CreateHexagonalGrid(int rows, int columns)
+        public async Task LoadKarakterekAsync()
         {
-            double hexRadius = 40; // Hexagon sugara
-            double hexWidth = Math.Sqrt(3) * hexRadius; // Hexagon szélessége
-            double hexHeight = 2 * hexRadius; // Hexagon magassága
-            double horizontalSpacing = hexWidth; // Vízszintes eltolás
-            double verticalSpacing = hexHeight * 0.75; // Függőleges eltolás (csökkentett)
-
-            // A Canvas méretének beállítása
-            double canvasWidth = columns * horizontalSpacing + hexWidth;
-            double canvasHeight = rows * verticalSpacing + hexHeight;
-            HexGridCanvas.Width = canvasWidth;
-            HexGridCanvas.Height = canvasHeight;
-
-            for (int row = 0; row < rows; row++)
+            try
             {
-                for (int col = 0; col < columns; col++)
+                var response = await sharedClient.GetAsync("KarakterClass");
+                response.EnsureSuccessStatusCode();
+
+                var json = await response.Content.ReadAsStringAsync();
+                var karakter = JsonConvert.DeserializeObject<List<CsapatepitoKarakterDTO>>(json);
+                KarakterLista = karakter;
+                CharacterListBox.ItemsSource = KarakterLista;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hiba történt: " + ex.Message);
+            }
+        }
+
+        private void CharacterListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (CharacterListBox.SelectedItems.Count > 0)
+            {
+                var selectedCharacters = new List<CsapatepitoKarakterDTO>();
+                foreach (var item in CharacterListBox.SelectedItems)
                 {
-                    // Az X és Y koordináták meghatározása
-                    double x = col * horizontalSpacing;
-
-                    // Minden második sort vízszintesen eltolunk
-                    if (row % 2 == 1)
+                    if (item is CsapatepitoKarakterDTO character)
                     {
-                        x += hexWidth / 2; // Elmozdítás az oszlopoknál
+                        selectedCharacters.Add(character);
                     }
-
-                    double y = row * verticalSpacing; // Függőleges eltolás változatlan
-
-                    var hexCell = new HexCellDTO
-                    {
-                        Id = _hexCells.Count + 1,
-                        X = col,
-                        Y = row,
-                        CharacterId = null // Alapértelmezetten nincs karakter
-                    };
-
-                    _hexCells.Add(hexCell);
-                    DrawHexagon(x, y, hexRadius, hexCell);
                 }
+
+                SelectedCharacterItemsControl.ItemsSource = selectedCharacters;
+                CharacterDetailBorder.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                CharacterDetailBorder.Visibility = Visibility.Collapsed;
             }
         }
 
+   
 
-        private void DrawHexagon(double x, double y, double radius, HexCellDTO hexCell)
-        {
-            Polygon hexagon = new Polygon
-            {
-                Stroke = Brushes.Black,
-                StrokeThickness = 1,
-                Fill = Brushes.LightBlue,
-                Tag = hexCell // Hozzáadjuk a hexCell objektumot a polygon tag-jához
-            };
-
-            PointCollection points = new PointCollection();
-
-            // Hexagon pontjainak kiszámítása a szög elforgatásával
-            for (int i = 0; i < 6; i++)
-            {
-                double angle = Math.PI / 3 * i - Math.PI / 6; // Elforgatás 30 fokkal
-                double px = x + radius * Math.Cos(angle);
-                double py = y + radius * Math.Sin(angle);
-                points.Add(new Point(px, py));
-            }
-
-            hexagon.Points = points;
-
-            hexagon.MouseDown += Hexagon_MouseDown; // Kattintás kezelése
-
-            HexGridCanvas.Children.Add(hexagon);
-        }
-
-
-        private void Hexagon_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            var hexagon = sender as Polygon;
-            var hexCell = hexagon.Tag as HexCellDTO;
-
-            if (hexCell != null)
-            {
-                // Ha a hexagonra kattintunk, megnyitunk egy karakter kiválasztó felületet
-                MessageBox.Show($"Hex {hexCell.X},{hexCell.Y} - CharacterID: {hexCell.CharacterId}");
-            }
-        }
-
-        private void LoadBoardData()
-        {
-            // Itt kell betöltened az adatbázisból a karaktereket (Board_hexese tábla alapján)
-            // Az adatok betöltése után frissíteni kell a hexagonokat a karakterekkel.
-            foreach (var hexCell in _hexCells)
-            {
-                // Betöltés a Board_hexese táblából az adott hex_x, hex_y alapján
-                var characterId = GetCharacterForCell(hexCell.X, hexCell.Y);
-                hexCell.CharacterId = characterId;
-            }
-        }
-
-        private int? GetCharacterForCell(int x, int y)
-        {
-            // Itt kell az adatbázisból lekérni a karakterek ID-ját a hex_x és hex_y alapján
-            // Ha nincs karakter, akkor null-t kell visszaadni
-            return null; // Csak példa, itt kell az adatbázisból lekérni az adatokat
-        }
-
-        // Táblázat mentése
-        private void SaveBoardData()
-        {
-            foreach (var hexCell in _hexCells)
-            {
-                SaveHexCellData(hexCell);
-            }
-        }
-
-        private void SaveHexCellData(HexCellDTO hexCell)
-        {
-            // Az adatokat elmenthetjük az adatbázisba a Board_hexese táblába
-            // Itt kell az adatbázis műveletet elvégezni, például ADO.NET-tel
-        }
 
         private void FoOldalBTN_Click(object sender, RoutedEventArgs e)
         {
@@ -208,15 +147,7 @@ namespace Karbantarto.Windows
             }
         }
 
-        private void TablaMentesBTN_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void TablaTorlesBTN_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
+   
     }
 }
 
